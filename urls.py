f41ab40e911f1672f7fba3f78819282b04e40a5d
moneykrishnaeducation <moneykrishnaeducation@gmail.com>
2025-12-12 12:43:59 +0530

    urls reload issue fixed

--- urls.py
+++ urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for rms project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
@@ -22,17 +6,9 @@ from core.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index'),
     path('api/', include('core.urls')),
-]
-
-# ⭐ Required for React Router page refresh (DO NOT REMOVE)
-urlpatterns += [
-    re_path(r'^.*$', index),
+    re_path(r'^(?!api/).*$', index, name='index'),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(
-        settings.STATIC_URL,
-        document_root=settings.STATICFILES_DIRS[0] if settings.STATICFILES_DIRS else 'static'
-    )
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0] if settings.STATICFILES_DIRS else 'static')
